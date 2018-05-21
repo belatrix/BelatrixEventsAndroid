4c00e3029692f6e2a7a54c419f72f04e6d7a0f7c
Luis Miguel Burgos <lburgos@belatrixsf.com>
2018-05-21 00:40:13 -0500

    Cambio de colores e imagenes. Actualización de imagenes de colaboradores.

--- app/src/main/java/com/belatrix/events/presentation/ui/activities/SplashActivity.java
+++ app/src/main/java/com/belatrix/events/presentation/ui/activities/SplashActivity.java
@@ -1,28 +1,29 @@
 /* The MIT License (MIT)
-* Copyright (c) 2016 BELATRIX
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
+ * Copyright (c) 2016 BELATRIX
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.belatrix.events.presentation.ui.activities;
 
 import android.os.Bundle;
 
 import com.belatrix.events.BuildConfig;
+import com.belatrix.events.R;
 import com.belatrix.events.di.component.UIComponent;
 import com.belatrix.events.presentation.ui.base.BelatrixBaseActivity;
 import com.belatrix.events.utils.cache.Cache;
@@ -45,6 +46,7 @@ public class SplashActivity extends BelatrixBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
         checkAppUpdated();
         if (cache.isFirstTime()) {
             startActivity(CitySelectionActivity.makeIntent(this));
@@ -55,9 +57,9 @@ public class SplashActivity extends BelatrixBaseActivity {
         finish();
     }
 
-    private void checkAppUpdated(){
+    private void checkAppUpdated() {
         int versionCode = cache.getVersionCode();
-        if(versionCode != BuildConfig.VERSION_CODE) {
+        if (versionCode != BuildConfig.VERSION_CODE) {
             //on app updated
             cache.deleteVotes();
         }
